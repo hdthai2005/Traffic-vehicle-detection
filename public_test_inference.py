3d2cb53467ae9e13f50c7ff9c1639d28ddfb58e3
hdthai2005 <hdthai2005@gmail.com>
2024-10-25 22:02:24 +0700

    sdsds

---
+++ public_test_inference.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+import os
+
+if __name__ == '__main__':
+    model = YOLO('runs/detect/train4/weights/best.pt')
+    test_files = os.listdir('public_test')
+
+    with open("predict.txt", 'w') as file:
+        for test_file in test_files:
+            predictions = model(f"public_test/{test_file}", save_txt=None)
+            for idx, prediction in enumerate(predictions[0].boxes.xywhn):
+                cls = int(predictions[0].boxes.cls[idx].item())
+                conf = predictions[0].boxes.conf[idx].item()
+                file.write(
+                    f"{test_file} {cls} {prediction[0].item()} {prediction[1].item()} {prediction[2].item()} {prediction[3].item()} {conf}\n")
